76485368cd1ab5bbaf758c6e78b05a112af3a939
inovatormatin <manojgeetparmar@gmail.com>
2022-06-06 12:20:59 +0530

    New: Dashboard architecture

--- src/components/header/AppHeaderDropdown.js
+++ src/components/header/AppHeaderDropdown.js
@@ -1,23 +1,25 @@
 import React from 'react'
-import { CAvatar, CDropdown, CDropdownToggle } from '@coreui/react'
+import {
+  CAvatar,
+  CDropdown,
+  CDropdownToggle,
+  CDropdownHeader,
+  CDropdownItem,
+  CDropdownMenu,
+} from '@coreui/react'
 // CBadge,
 // CDropdownDivider,
-// CDropdownHeader,
-// CDropdownItem,
-// CDropdownMenu,
 
-// import {
-//   cilBell,
-//   cilCreditCard,
-//   cilCommentSquare,
-//   cilEnvelopeOpen,
-//   cilFile,
-//   cilLockLocked,
-//   cilSettings,
-//   cilTask,
-//   cilUser,
-// } from '@coreui/icons'
-// import CIcon from '@coreui/icons-react'
+import { cilAccountLogout } from '@coreui/icons'
+// cilBell,
+// cilCommentSquare,
+// cilEnvelopeOpen,
+// cilFile,
+// cilLockLocked,
+// cilSettings,
+// cilTask,
+// cilUser,
+import CIcon from '@coreui/icons-react'
 
 import avatar8 from './../../assets/images/avatars/default.png'
 
@@ -27,16 +29,13 @@ const AppHeaderDropdown = () => {
       <CDropdownToggle placement="bottom-end" className="py-0" caret={false}>
         <CAvatar src={avatar8} size="md" />
       </CDropdownToggle>
-      {/* <CDropdownMenu className="pt-0" placement="bottom-end">
+      <CDropdownMenu className="pt-0" placement="bottom-end">
         <CDropdownHeader className="bg-light fw-semibold py-2">Account</CDropdownHeader>
-        <CDropdownItem href="#">
-          <CIcon icon={cilBell} className="me-2" />
-          Updates
-          <CBadge color="info" className="ms-2">
-            42
-          </CBadge>
+        <CDropdownItem href="/">
+          <CIcon icon={cilAccountLogout} className="me-2" />
+          Logout
         </CDropdownItem>
-        <CDropdownItem href="#">
+        {/* <CDropdownItem href="#">
           <CIcon icon={cilEnvelopeOpen} className="me-2" />
           Messages
           <CBadge color="success" className="ms-2">
@@ -84,8 +83,8 @@ const AppHeaderDropdown = () => {
         <CDropdownItem href="#">
           <CIcon icon={cilLockLocked} className="me-2" />
           Lock Account
-        </CDropdownItem>
-      </CDropdownMenu> */}
+        </CDropdownItem> */}
+      </CDropdownMenu>
     </CDropdown>
   )
 }
--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import {
   CButton,
   CCard,
@@ -15,24 +15,44 @@ import {
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 import { cilLockLocked, cilUser } from '@coreui/icons'
+import './login.css'
 
 const Login = () => {
+  const [stdInput, setStdInput] = useState({
+    name: null,
+    password: null,
+  })
+  let navigate = useNavigate()
+  const loginuser = () => {
+    if (stdInput.name !== null && stdInput.password !== null) {
+      navigate('/dashboard')
+    } else {
+      alert('Please enter username with correct password')
+    }
+  }
+
   return (
-    <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
+    <div className="bg-light min-vh-100 d-flex flex-row align-items-center loginPage">
       <CContainer>
         <CRow className="justify-content-center">
           <CCol md={8}>
             <CCardGroup>
-              <CCard className="p-4">
+              <CCard className="p-4 bg-opacity-75 bg-dark text-white">
                 <CCardBody>
                   <CForm>
                     <h1>Login</h1>
-                    <p className="text-medium-emphasis">Sign In to your account</p>
+                    <p className="text-white">Sign In to your account</p>
                     <CInputGroup className="mb-3">
                       <CInputGroupText>
                         <CIcon icon={cilUser} />
                       </CInputGroupText>
-                      <CFormInput placeholder="Username" autoComplete="username" />
+                      <CFormInput
+                        placeholder="Username"
+                        autoComplete="username"
+                        onChange={(e) => {
+                          setStdInput({ ...stdInput, name: e.target.value })
+                        }}
+                      />
                     </CInputGroup>
                     <CInputGroup className="mb-4">
                       <CInputGroupText>
@@ -42,16 +62,19 @@ const Login = () => {
                         type="password"
                         placeholder="Password"
                         autoComplete="current-password"
+                        onChange={(e) => {
+                          setStdInput({ ...stdInput, password: e.target.value })
+                        }}
                       />
                     </CInputGroup>
                     <CRow>
                       <CCol xs={6}>
-                        <CButton color="primary" className="px-4">
+                        <CButton color="primary" className="px-4" onClick={() => loginuser()}>
                           Login
                         </CButton>
                       </CCol>
-                      <CCol xs={6} className="text-right">
-                        <CButton color="link" className="px-0">
+                      <CCol xs={6} className="text-end">
+                        <CButton color="link" className="px-0 text-white">
                           Forgot password?
                         </CButton>
                       </CCol>
@@ -59,7 +82,7 @@ const Login = () => {
                   </CForm>
                 </CCardBody>
               </CCard>
-              <CCard className="text-white bg-primary py-5" style={{ width: '44%' }}>
+              {/* <CCard className="text-white bg-primary py-5" style={{ width: '44%' }}>
                 <CCardBody className="text-center">
                   <div>
                     <h2>Sign up</h2>
@@ -74,7 +97,7 @@ const Login = () => {
                     </Link>
                   </div>
                 </CCardBody>
-              </CCard>
+              </CCard> */}
             </CCardGroup>
           </CCol>
         </CRow>
